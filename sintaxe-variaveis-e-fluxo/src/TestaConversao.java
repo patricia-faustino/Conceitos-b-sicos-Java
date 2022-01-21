21ca9cebfb0277262115011edcb15f7c4652cb5e
PATRICIA FAUSTINO DE DE JESUS <patricia.faustino@iteris.com.br>
2022-01-20 21:35:00 -0300

    lidando com variaveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,19 @@
+
+public class TestaConversao {
+	public static void main(String[] args) {
+		double salario = 120.50;
+		int valor = (int) salario;
+		System.out.println(valor);
+		
+		float pontoFlutuante = 3.14f;
+		
+		long numeroGrande = 344444444444444445L;
+		short valorPequeno = 2131;
+		byte valorpequeninho = 127;
+		
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1 + valor2;
+		System.out.println("Particularidades de operações com número decimal: " + total);
+	}
+}
