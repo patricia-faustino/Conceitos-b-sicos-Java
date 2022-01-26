b8669a08ea43bae1b65d3477107dc37c3f7d5f86
PATRICIA FAUSTINO DE DE JESUS <patricia.faustino@iteris.com.br>
2022-01-26 11:54:08 -0300

    super e reescrita de metodos

---
+++ bytebank-herdado/src/TesteGerente.java
@@ -0,0 +1,18 @@
+
+public class TesteGerente {
+	public static void main(String[] args) {
+		Gerente gerente = new Gerente();
+		
+		gerente.setNome("Fernanda Souza");
+		gerente.setCpf("333.333.222-44");
+		gerente.setSenha(123);
+		gerente.setSalario(8000.00);
+		
+		System.out.println(gerente.getNome());
+		System.out.println(gerente.getCpf());
+		
+		System.out.println(gerente.autentica(123));
+		
+		System.out.println(gerente.getSalario());
+	}
+}
