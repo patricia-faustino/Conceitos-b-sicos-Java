ca062ae69b9081d50fa4f35ffdc5df42becd4eac
PATRICIA FAUSTINO DE DE JESUS <patricia.faustino@iteris.com.br>
2022-01-27 17:47:56 -0300

    lancando excecoes

---
+++ java-pilha/src/FluxoComTratamento.java
@@ -0,0 +1,28 @@
+
+public class FluxoComTratamento {
+
+	public static void main(String[] args) {
+		System.out.println("Início do main");
+		try {
+			metodo1();
+		} catch (ArithmeticException  | NullPointerException ex) {
+			String mensagemException = ex.getMessage();
+			System.out.println("Exception " + mensagemException);
+			ex.printStackTrace();
+		} 
+		System.out.println("Fim do main");
+	}
+
+	private static void metodo1() {
+		System.out.println(" Início do metodo1");
+		metodo2();
+		System.out.println("Fim do metodo1");
+	}
+
+	private static void metodo2() {
+		System.out.println("Início do metodo2");
+		 
+		throw new ArithmeticException("Verificar campo");
+		//System.out.println("Fim do metodo2");
+	}
+}
