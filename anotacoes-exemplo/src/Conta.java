10eac16823ce0e70ab98aa981aaf600ddd7ccf6c
PATRICIA FAUSTINO DE DE JESUS <patricia.faustino@iteris.com.br>
2022-01-27 17:13:41 -0300

    tratamento de excecoes

---
+++ anotacoes-exemplo/src/Conta.java
@@ -0,0 +1,78 @@
+public class Conta {
+	
+	@DoublePositivo
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+
+	private static int total;
+
+	public Conta(int agencia, int numero) {
+		Conta.total++;
+		this.agencia = agencia;
+		this.numero = numero;
+		this.saldo = 100;
+	}
+	
+	public Conta() {
+		Conta.total++;
+	}
+
+	public void deposit(double valor) {
+		this.saldo += valor;
+	}
+
+	public boolean withdraw(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean transfer(Conta contaDestino, double valor) {
+		if (this.saldo >= 0) {
+			withdraw(valor);
+			contaDestino.deposit(valor);
+			return true;
+		}
+		return false;
+	}
+	
+	 public double getSaldo() {
+		return saldo;
+	}
+	 
+		public int getAgencia() {
+			return agencia;
+		}
+
+		public void setAgencia(int agencia) {
+			this.agencia = agencia;
+		}
+
+		public int getNumero() {
+			return numero;
+		}
+
+		public void setNumero(int numero) {
+			this.numero = numero;
+		}
+		
+
+		public Cliente getTitular() {
+			return titular;
+		}
+
+		public void setTitular(Cliente titular) {
+			this.titular = titular;
+		}
+		
+
+		public static int getTotal() {
+			return total;
+		}
+		
+
+}
