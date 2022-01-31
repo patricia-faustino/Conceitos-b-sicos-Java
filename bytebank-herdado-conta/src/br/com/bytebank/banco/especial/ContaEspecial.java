7766828d1e7911daa864a6bef263c7402599897f
PATRICIA FAUSTINO DE DE JESUS <patriciafaustinobr@gmail.com>
2022-01-30 21:27:05 -0300

    trabalhando com modificadores

---
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/especial/ContaEspecial.java
@@ -0,0 +1,14 @@
+package br.com.bytebank.banco.especial;
+
+import br.com.bytebank.banco.modelo.Conta;
+
+public class ContaEspecial extends Conta {
+
+	@Override
+	public void deposit(double valor) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/SaldoInsuficienteException.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/SaldoInsuficienteException.java
@@ -4,5 +4,4 @@ public class SaldoInsuficienteException extends Exception {
 	public SaldoInsuficienteException(String mensagem) {
 		super(mensagem);
 	}
-
 }
